45fcb850e9e1e4487eef68ff34abeb62b83f7ad6
steffanimarcela <102896391+steffanimarcela@users.noreply.github.com>
2023-11-01 16:07:12 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,18 +1,9 @@
-=import 'package:aplikasi_flutter_pertamaku/hello_world.dart';
-import 'package:flutter/material.dart';
+import 'package:aplikasi_flutter_pertamaku/column_widget.dart';
+ import 'package:flutter/material.dart';
 
  void main() {
  runApp(const MyApp());
  }
 
-class MyApp extends StatelessWidget {
+ class MyApp extends StatelessWidget {
  const MyApp({Key? key}) : super(key: key);
-
- @override
- Widget build(BuildContext context) {
- Return const MaterialApp(
- title: "Aplikasi Flutter Pertama",
- home: HelloWorld(),
- );
- }
- }
